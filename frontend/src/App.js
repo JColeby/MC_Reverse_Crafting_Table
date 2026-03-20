1b824a819fc28cb2b4b9192d62b146fc785a9ee8
Blerfx2 <B10awsomeme2011@gmail.com>
2026-03-20 16:50:13 -0600

    Added background and began work on forms for first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <form>
+          <label for="itemIDs">Items</label>
+          <label for="quantity">#</label><br></br>
+          <select id="itemIDs" name="itemIDs">
+            <option>Stone Sword</option>
+          </select>
+          <input type="number" id="quantity" name="quantity"></input><br></br>
+          <input type="submit" value="Add item" name="addItem"></input>
+          <input type="submit" value="Find raw materials" name="reverseSearch"></input>
+          <input type="submit" value="What can I craft?" name="forwardSearch"></input>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// Default Code
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +35,4 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        </a> */}
