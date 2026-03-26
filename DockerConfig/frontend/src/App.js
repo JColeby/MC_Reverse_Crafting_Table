44451481fd052d535bfeb71e2acbb70193d273bf
blerfx2 <blerfx@byu.edu>
2026-03-26 16:30:22 -0600

    Some CSS changes

--- DockerConfig/frontend/src/App.js
+++ DockerConfig/frontend/src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form>
+        <form className="App-form">
           <label for="itemIDs">Items</label>
           <label for="quantity">#</label><br></br>
           <select id="itemIDs" name="itemIDs">
             <option>Stone Sword</option>
           </select>
-          <input type="number" id="quantity" name="quantity"></input><br></br>
+          <input className="App-quantity" type="number" id="quantity" name="quantity"></input><br></br>
           <input type="submit" value="Add item" name="addItem"></input>
           <input type="submit" value="Find raw materials" name="reverseSearch"></input>
           <input type="submit" value="What can I craft?" name="forwardSearch"></input>
