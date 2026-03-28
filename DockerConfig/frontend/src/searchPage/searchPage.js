35da2f590064dc0f49206266203563cc00ed1ea4
blerfx2 <blerfx@byu.edu>
2026-03-27 21:59:37 -0600

    Updated SearchPage to parse JSON into options that a user can choose

--- DockerConfig/frontend/src/searchPage/searchPage.js
+++ DockerConfig/frontend/src/searchPage/searchPage.js
@@ -1,14 +1,36 @@
 
 
+const jsonobj = {
+  "item1": 1,
+  "item2": 2,
+  "item3": 3,
+  "minecraft:stone_sword":4
+};
 
 export function SearchPage() {
     return (
         <form className="App-form">
           <label htmlFor="itemIDs">Items</label>
           <label htmlFor="quantity">#</label><br></br>
+          
           <select id="itemIDs" name="itemIDs">
-            <option>Stone Sword</option>
+            {Object.entries(jsonobj).map(([key, value]) => {
+            // Remove "minecraft:" prefix
+            const rawName = key.split('minecraft:')[1] || key;
+            
+            // Converts name to be human readable"
+            const readableName = rawName
+              .split('_')                   
+              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(' ');                    
+            
+            return (
+              <option key={key} value={value}>{readableName}</option>
+            );
+          })}
+
           </select>
+
           <input className="App-quantity" type="number" id="quantity" name="quantity"></input><br></br>
           <input type="submit" value="Add item" name="addItem"></input>
           <input type="submit" value="Find raw materials" name="reverseSearch"></input>
