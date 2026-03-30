41b093f399ea2b11b8056327d4a5de49ef1823d7
blerfx2 <blerfx@byu.edu>
2026-03-30 16:35:01 -0600

    Updated some css and the searchPage to look nicer. searchPage is now ready to start sending information to APIs

--- DockerConfig/frontend/src/searchPage/searchPage.js
+++ DockerConfig/frontend/src/searchPage/searchPage.js
@@ -1,62 +1,113 @@
-
-
-
-
-// TODO: Make handleAdd output information on what the user has queried
-
 import { useState } from "react";
 
 export function SearchPage({ craftIDs }) {
-    const [requestedItems, setRequestedItems] = useState([]);
-    const [count, setCount] = useState(0);
-    const [largeLoad, setLargeLoad] = useState('');
+  const [requestedItems, setRequestedItems] = useState([]);
+  const [largeLoad, setLargeLoad] = useState("");
+  const [itemID, setItemID] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
-    function handleAdd(id, qunatity) {
-      if (count >= 10) {
-        requestedItems.push({
-          itemid: id,
-          ingredientquantity: qunatity,
-        });
-      }
-      else {
-        setLargeLoad('Only 10 items may be requested')
-      };
-      <p>
-        { largeLoad } <br></br>
-        { requestedItems.map }
-      </p>
+  function handleAdd() {
+    if (requestedItems.length >= 10) {
+      setLargeLoad("Only 10 items may be requested");
+      return;
     }
 
-    return (
-        <form className="App-form">
-          <label htmlFor="itemIDs">Items</label>
-          <label htmlFor="quantity">#</label><br></br>
-          
-          <select id="itemIDs" name="itemIDs">
-            {Object.entries(craftIDs).map(([key, value]) => {
-            // This function will take all the item ids and their names
-            // and make a human readable dropdown for users to use
+    // Find name from craftIDs using selected ID
+    const selectedName = Object.entries(craftIDs).find(
+      ([key, value]) => value === Number(itemID)
+    )?.[0]?.split("minecraft:")[1] || "Unknown";
 
-            // Remove "minecraft:" prefix
-            const rawName = key.split('minecraft:')[1] || key;
-            
-            // Converts name to be human readable"
-            const readableName = rawName
-              .split('_')                   
-              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-              .join(' ');                    
-            
-            return (
-              <option key={key} value={value}>{readableName}</option>
-            );
-          })}
+    // Make name human readable
+    const readableName = selectedName
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
 
-          </select>
+    setLargeLoad("");
+    setRequestedItems((prev) => [
+      ...prev,
+      {
+        itemid: Number(itemID),              
+        itemname: readableName,      
+        ingredientquantity: Number(quantity),
+      },
+    ]);
 
-          <input className="App-quantity" type="number" max="10" id="quantity" name="quantity"></input><br></br>
-          <input type="submit" value="Add item" name="addItem"></input>
-          <input type="submit" value="Find raw materials" name="reverseSearch"></input>
-          <input type="submit" value="What can I craft?" name="forwardSearch"></input>
-        </form>
-    )
+    // Reset form
+    setItemID("");
+    setQuantity("");
+  }
+
+  function handleCraft(e) {
+    e.preventDefault();
+    // Send to API or navigate with requestedItems
+    console.log("Crafting with:", requestedItems);
+    // Example API data format:
+    // const apiData = requestedItems.map(({ itemid, ingredientquantity }) => ({
+    //   itemid,
+    //   ingredientquantity,
+    // }));
+    // navigate("/fsr", { state: { requestedItems } });
+  }
+
+  function handleReverseSearch(e) {
+    e.preventDefault();
+    console.log("Reverse search with:", requestedItems);
+  }
+
+  return (
+    <form className="App-form">
+
+      <div className="form-labels">
+      <label htmlFor="itemIDs" >Items</label>
+      <label htmlFor="quantity">#</label>
+      </div>
+
+      <div className="form-inputs-row">
+      <select id="itemIDs" name="itemIDs" value={itemID}
+        onChange={(e) => setItemID(e.target.value)}>
+
+        <option value="">Select an item</option>
+        {Object.entries(craftIDs).map(([key, value]) => { // Makes dropdown that is human readable
+          const rawName = key.split("minecraft:")[1] || key;
+          const readableName = rawName
+            .split("_")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ");
+
+          return (
+            <option key={key} value={value}>
+              {readableName}
+            </option>
+          );
+        })}
+      </select>
+
+      
+      <input
+        className="App-quantity" type="number" id="quantity" name="quantity" value={quantity}
+        onChange={(e) => setQuantity(e.target.value)} min="1"
+      />
+      </div>
+      <br />
+
+      <button type="button" onClick={handleAdd}> Add item </button>
+
+      <button type="submit" onClick={handleCraft}> What can I craft? </button>
+
+      <button type="submit" onClick={handleReverseSearch}> Find raw materials </button>
+
+      <p>
+        {largeLoad && <strong>{largeLoad}</strong>}
+        <br />
+        Requested Items ({requestedItems.length}/10):
+        <br />
+        {requestedItems.map((item, index) => (
+          <div key={index}>
+            Item: {item.itemname} Quantity: {item.ingredientquantity}
+          </div>
+        ))}
+      </p>
+    </form>
+  );
 }
