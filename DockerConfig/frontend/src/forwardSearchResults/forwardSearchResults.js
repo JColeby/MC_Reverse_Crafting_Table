4f7b4379e1425083fbb9e79454a814d7ac14fdb4
blerfx2 <blerfx@byu.edu>
2026-03-31 12:05:40 -0600

    Commented out some test data for now.

--- DockerConfig/frontend/src/forwardSearchResults/forwardSearchResults.js
+++ DockerConfig/frontend/src/forwardSearchResults/forwardSearchResults.js
@@ -1,32 +1,32 @@
 
-const searchResTest = {
-  "recipies": [
-    {
-      "recipeid": 1,
-      "recipeitemid": 101,
-      "ingredientitemid": 202,
-      "recipetype": "crafting",
-      "pattern": "XXX",
-      "patternkey": "X"
-    },
-    {
-      "recipeid": 2,
-      "recipeitemid": 102,
-      "ingredientitemid": 204,
-      "recipetype": "smelting",
-      "pattern": "X_X",
-      "patternkey": "X"
-    },
-    {
-      "recipeid": 2,
-      "recipeitemid": 102,
-      "ingredientitemid": 205,
-      "recipetype": "smelting",
-      "pattern": "X_X",
-      "patternkey": "_"
-    }
-  ]
-}
+// const searchResTest = {
+//   "recipies": [
+//     {
+//       "recipeid": 1,
+//       "recipeitemid": 101,
+//       "ingredientitemid": 202,
+//       "recipetype": "crafting",
+//       "pattern": "XXX",
+//       "patternkey": "X"
+//     },
+//     {
+//       "recipeid": 2,
+//       "recipeitemid": 102,
+//       "ingredientitemid": 204,
+//       "recipetype": "smelting",
+//       "pattern": "X_X",
+//       "patternkey": "X"
+//     },
+//     {
+//       "recipeid": 2,
+//       "recipeitemid": 102,
+//       "ingredientitemid": 205,
+//       "recipetype": "smelting",
+//       "pattern": "X_X",
+//       "patternkey": "_"
+//     }
+//   ]
+// }
 
 /* We want this to take in the JSON object, make a new and more coherent one,
 and then format it to display the data in a 3x3 grid with some text next to it
